fbdc89be7476f7f9e94305e2806ecedf4d3ed12a
Victor Jimenez <v.jimenez-gomez@hotmail.com>
2022-11-21 16:14:34 +0100

    Subscribe store and temperature random

--- src/components/Freezer/Freezer.js
+++ src/components/Freezer/Freezer.js
@@ -1,30 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Panel from '../Panel/Panel';
-import FreezerFlavor from '../FreezerFlavor/FreezerFlavor';
+import Panel from "../Panel/Panel";
+import FreezerFlavor from "../FreezerFlavor/FreezerFlavor";
 
-import store from '../../store'
+import store from "../../store";
+import { actions as actionsFreezer } from "../../ducks/freezer";
 
 class Freezer extends Component {
   state = {
     flavors: store.getState().freezer.icecreams,
-    temperature: store.getState().freezer.temperature
+    temperature: store.getState().freezer.temperature,
   }
+
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() => {
+      this.setState({
+        flavors: store.getState().freezer.icecreams,
+        temperature: store.getState().freezer.temperature
+      });
+    });
+    setInterval(() => {
+      const randomTemp = -Math.round(Math.random()*10);
+      store.dispatch(actionsFreezer.updateTemperature(randomTemp))
+    }, 4000);
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
   render() {
-    console.log(this.state)
+    console.log(this.state);
     const flavors = this.state.flavors;
-    const freezerFlavors = Object.keys(flavors).map( flavorsName => 
-      <FreezerFlavor 
-        key={flavorsName} 
-        flavorName={flavorsName} 
-        scoops={flavors[flavorsName]} />);
+    const freezerFlavors = Object.keys(flavors).map((flavorsName) => (
+      <FreezerFlavor
+        key={flavorsName}
+        flavorName={flavorsName}
+        scoops={flavors[flavorsName]}
+      />
+    ));
     return (
       <Panel title={`Freezer (${this.state.temperature}°C)`}>
-        { freezerFlavors }
+        {freezerFlavors}
       </Panel>
     );
   }
 }
 
 export default Freezer;
-
--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import App from './components/App/App'
 
 import { createRoot } from "react-dom/client";
 
+import * as flavors from './constants/icecreams_flavors';
+import { actions as actionsFreezer } from "./ducks/freezer";
+import store from "./store";
 
+setInterval(() => {
+    store.dispatch(actionsFreezer.addIceCream(flavors.FABADA, 5))
+}, 3500);
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
